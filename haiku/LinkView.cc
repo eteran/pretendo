e20165a4ea0f8edf1e3fc8b70f2848101a22e7cd
eli <eli.dayan@gmail.com>
2013-04-09 08:06:05 -0400

    links act more like links now

--- haiku/LinkView.cc
+++ haiku/LinkView.cc
@@ -47,22 +47,35 @@ LinkView::MouseUp (BPoint point)
 {
 	be_roster->Launch("text/html", 1, &fLink);
 	
+	SetHighColor(0, 0, 255);
+	BRect frame(Frame());
+	frame.OffsetTo(B_ORIGIN);
+	BStringView::Draw(frame);
+	
 	BStringView::MouseUp(point);
 }
+
+
+void
+LinkView::MouseDown (BPoint point)
+{
+	SetHighColor(255, 0, 0);
+	
+	BRect frame(Frame());
+	frame.OffsetTo(B_ORIGIN);
+	BStringView::Draw(frame);
+	
+	BStringView::MouseDown (point);
+}
+
 
 void
 LinkView::MouseMoved (BPoint point, uint32 transit, const BMessage *message)
 {
-	switch (transit) {
-		case B_ENTERED_VIEW:
-			//if(GetTextRect().Contains(point)) {
-    			be_app->SetCursor(fLinkCursor);
-			//}
-			break;
-			
-		case B_EXITED_VIEW:
-			be_app->SetCursor(B_HAND_CURSOR);
-			break;
+	if (transit == B_ENTERED_VIEW) {
+		be_app->SetCursor(fLinkCursor);
+	} else if (transit == B_EXITED_VIEW) {
+		be_app->SetCursor(B_HAND_CURSOR);
 	}
 	
 	BStringView::MouseMoved(point, transit, message);
@@ -74,56 +87,3 @@ LinkView::Draw (BRect updateRect)
 	BStringView::Draw(updateRect);
 }
 
-
-BRect LinkView::GetTextRect()
-{
- // This function will return a BRect that contains only the text
- // and the underline, so the mouse can change and the link will
- // be activated only when the mouse is over the text itself, not
- // just within the view.
- 
- // Note:  We'll use bounding boxes, because they are the most
- //        accurate, and since the user is interacting with the
- //        view, off-by-one-pixel errors look bad.
- 	const char *textArray[1];
- 	textArray[0]=Text();
- 
- 	escapement_delta delta;
- 	delta.nonspace=0;
- 	delta.space=0;
- 	escapement_delta escapements[1];
- 	escapements[0]=delta;
- 
- 	BRect returnMe;
- 	BRect rectArray[1];
- 	rectArray[0]=returnMe;
- 
- 	BFont font;
- 	GetFont(&font);
- 	font.GetBoundingBoxesForStrings(textArray, 1, B_SCREEN_METRIC, escapements, 					rectArray);
-
- 	BRect frame=Frame();
- 	frame.OffsetTo(B_ORIGIN);
- 	returnMe=rectArray[0];
- 
- 	// Get the height of the current font.
- 	font_height height;
- 	GetFontHeight(&height);
- 	float descent=height.descent;
- 
- 	// Account for rounding of the floats when drawn to avoid
- 	// one-pixel-off errors.
- 	float lowerBound=0;
- 	if((((int) descent)*2)!=((int) (descent*2))) lowerBound=1;
- 
- 	// Adjust the bounding box to reflect where the text is in the view.
- 	returnMe.bottom+=1;
- 	returnMe.OffsetTo(B_ORIGIN);
- 	float rectHeight=returnMe.Height();
- 	returnMe.bottom=frame.bottom-descent;
- 	returnMe.top=returnMe.bottom-rectHeight;
- 	returnMe.bottom+=1+1;
- 	returnMe.OffsetBy(0.0, -(1+lowerBound));
-
- return returnMe;
-}
