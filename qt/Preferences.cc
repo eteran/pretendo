258aac3fe53b6c7f36f47419bfe591bbe440655a
Evan Teran <eteran@alum.rit.edu>
2013-05-19 21:52:04 -0400

    added sliders for all tuneable settings added a "restore defaults" button

--- qt/Preferences.cc
+++ qt/Preferences.cc
@@ -2,6 +2,8 @@
 #include "Preferences.h"
 #include "../Palette.h"
 #include <QPainter>
+#include <QAbstractButton>
+#include <cstdio>
 
 namespace {
 	const int palette_block_width  = 24;
@@ -12,7 +14,13 @@ namespace {
 //------------------------------------------------------------------------------
 // Name: Preferences
 //------------------------------------------------------------------------------
-Preferences::Preferences(QWidget *parent, Qt::WindowFlags f) : QDialog(parent, f) {
+Preferences::Preferences(QWidget *parent, Qt::WindowFlags f) : QDialog(parent, f), 
+		current_saturation_(Palette::default_saturation), 
+		current_hue_(Palette::default_hue), 
+		current_contrast_(Palette::default_contrast), 
+		current_brightness_(Palette::default_brightness), 
+		current_gamma_(Palette::default_gamma) {
+		
 	ui_.setupUi(this);
 	ui_.palette->installEventFilter(this);
 }
@@ -33,13 +41,12 @@ bool Preferences::eventFilter(QObject *watched, QEvent *event) {
 		QPainter painter;
 		painter.begin(ui_.palette);
 
-
 		const rgb_color_t *const palette = Palette::NTSCPalette(
-			Palette::default_saturation,
-			Palette::default_hue,
-			Palette::default_contrast,
-			Palette::default_brightness,
-			Palette::default_gamma
+			current_saturation_,
+			current_hue_,
+			current_contrast_,
+			current_brightness_,
+			current_gamma_
 		);
 		
 		for(int x = 0; x < 16; ++x) {
@@ -59,3 +66,58 @@ bool Preferences::eventFilter(QObject *watched, QEvent *event) {
 	}
 	return false;
 }
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_hue_valueChanged(int value) {
+	current_hue_ = static_cast<float>(value) / 1000.0;
+	ui_.palette->update();
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_saturation_valueChanged(int value) {
+	current_saturation_ = static_cast<float>(value) / 100.0;
+	ui_.palette->update();
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_contrast_valueChanged(int value) {
+	current_contrast_ = static_cast<float>(value) / 100.0;
+	ui_.palette->update();
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_brightness_valueChanged(int value) {
+	current_brightness_ = static_cast<float>(value) / 100.0;
+	ui_.palette->update();
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_gamma_valueChanged(int value) {
+	current_gamma_ = static_cast<float>(value) / 100.0;
+	ui_.palette->update();
+}
+
+//------------------------------------------------------------------------------
+// Name: 
+//------------------------------------------------------------------------------
+void Preferences::on_buttonBox_clicked(QAbstractButton *button) {
+
+	if(ui_.buttonBox->standardButton(button) == QDialogButtonBox::RestoreDefaults) {
+		ui_.hue       ->setValue(Palette::default_hue        * 1000);
+		ui_.saturation->setValue(Palette::default_saturation * 100);
+		ui_.contrast  ->setValue(Palette::default_contrast   * 100);
+		ui_.brightness->setValue(Palette::default_brightness * 100);
+		ui_.gamma     ->setValue(Palette::default_gamma      * 100);
+	}
+}
+
--- qt/Preferences.h
+++ qt/Preferences.h
@@ -13,9 +13,20 @@ public:
 	
 protected Q_SLOTS:
 	bool eventFilter(QObject *watched, QEvent *event);
+	void on_hue_valueChanged(int value);
+	void on_saturation_valueChanged(int value);
+	void on_contrast_valueChanged(int value);
+	void on_brightness_valueChanged(int value);
+	void on_gamma_valueChanged(int value);
+	void on_buttonBox_clicked(QAbstractButton *button);
 	
 private:
 	Ui::Preferences ui_;
+	float current_saturation_;
+	float current_hue_;
+	float current_contrast_;
+	float current_brightness_;
+	float current_gamma_;
 };
 
 #endif
