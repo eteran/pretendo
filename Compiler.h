574ce7790c68eae7213020cc06a4c5751682ed82
Evan Teran <evan.teran@gmail.com>
2018-10-09 21:58:41 -0400

    better handling of compiler versions

--- Compiler.h
+++ Compiler.h
@@ -2,8 +2,14 @@
 #ifndef COMPILER_H_
 #define COMPILER_H_
 
-#define EXPECT(expr, val)  __builtin_expect(expr, val)
-#define LIKELY(expr)       EXPECT((expr), 1)
-#define UNLIKELY(expr)     EXPECT((expr), 0)
+#if defined(__GNUC__)
+	#define EXPECT(expr, val)  __builtin_expect(expr, val)
+	#define LIKELY(expr)       EXPECT((expr), 1)
+	#define UNLIKELY(expr)     EXPECT((expr), 0)
+#else
+	#define EXPECT(expr, val)  (expr)
+	#define LIKELY(expr)       (expr)
+	#define UNLIKELY(expr)     (expr)
+#endif
 
 #endif
