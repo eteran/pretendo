6cfff275f2a454f940c6b66b48ee48f1e5969863
Evan Teran <evan.teran@gmail.com>
2019-02-15 20:29:14 -0500

    minor improvments to build

--- Compiler.h
+++ Compiler.h
@@ -3,8 +3,8 @@
 #define COMPILER_H_
 
 #if defined(__GNUC__)
-    #define LIKELY(expr)       __builtin_expect(static_cast<bool>(expr), 1)
-    #define UNLIKELY(expr)     __builtin_expect(static_cast<bool>(expr), 0)
+    #define LIKELY(expr)       __builtin_expect(!!(expr), 1)
+    #define UNLIKELY(expr)     __builtin_expect(!!(expr), 0)
 #else
 	#define LIKELY(expr)       (expr)
 	#define UNLIKELY(expr)     (expr)
