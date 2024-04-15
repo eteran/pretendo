15f2da8d64c7aa052871d3fa4f4019a5be21a2b6
Evan Teran <evan.teran@gmail.com>
2024-04-15 18:56:20 -0400

    support hidpi

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -20,6 +20,12 @@ namespace {
 }
 
 int main(int argc, char *argv[]) {
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+	QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
 	QApplication app(argc, argv);
 
 	int fps              = 60;
